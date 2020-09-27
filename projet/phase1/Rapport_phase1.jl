165c7fdd46b072742058c75e3493b15119e10bff
louproul <70526480+louproul@users.noreply.github.com>
2020-09-27 17:01:54 -0400

    Merge the report with Elahe Main

--- projet/phase1/Rapport_phase1.jl
+++ projet/phase1/Rapport_phase1.jl
@@ -30,14 +30,6 @@ begin
 	filepath = joinpath(filepath, filename_stsp) 
 end
 
-# ╔═╡ b0f22570-00fc-11eb-1950-b95e857f6166
-for (root, dirs, files) in walkdir(@__DIR__)
-    println("Directories in $root")
-    for dir in dirs
-        println(joinpath(root, dir)) # path to directories
-    end
-end
-
 # ╔═╡ 1734aa10-00da-11eb-297e-fd626ae614e5
 graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 
@@ -50,38 +42,19 @@ Edges = Edge{Array{Float64,1}}[]
 # ╔═╡ b1329a90-00e0-11eb-1d3f-c9999ef689d7
 my_graph = Graph(filename_stsp,Nodes,Edges)
 
-# ╔═╡ 3d2bad10-00e7-11eb-383b-5b97be2d9f26
-Node("18",graph_nodes[18])
-
-# ╔═╡ f7244800-00db-11eb-1153-c3ed8515bf31
-begin
-	for (k, v) in graph_nodes
-		push!(Nodes,Node(string(k),v))
-	end
+# ╔═╡ 279400c0-0104-11eb-30f2-23b812fd577f
+for k = 1 : length(graph_edges)
+    new_node1 = Node(string(k),graph_nodes[k])
+    add_node!(my_graph, new_node1)
+    for j in graph_edges[k]
+        new_node2 = Node(string(j),graph_nodes[j])
+        edge_name = "("*string(k)*","*string(j)*")"
+        new_edge = Edge(edge_name, edges_weight[k,j], (new_node1 , new_node2))
+        add_edge!(my_graph, new_edge)
+    end
 end
 
-# ╔═╡ 27e84270-00dc-11eb-1264-3bde81f1f815
-Nodes
-
-# ╔═╡ 11aa7bd0-00dd-11eb-250a-673e7c88ac11
-Edge("test",(Nodes[1], Nodes[2]),parse(Int,edges_weight[1,2]))
-
-# ╔═╡ 27c87d10-00ea-11eb-232c-c3a8b25f4cf3
-Nodes[findfirst(x -> x.name == string(1),Nodes)]
-
-# ╔═╡ 39d75c80-00e8-11eb-1667-db4d90329d47
-for i in eachindex(graph_edges)
-	index_1 = findfirst(x -> x.name == string(i),Nodes)
-	for j in graph_edges[i]
-	index_2 = findfirst(x -> x.name == string(j),Nodes)	
-		push!(Edges,Edge("test",(Nodes[index_1],Nodes[index_2]),parse(Int,edges_weight[i,j])))
-	end
-end
-
-# ╔═╡ 91b20eb0-00dd-11eb-3c54-8b9b004e94e8
-Edges
-
-# ╔═╡ 6d4fc460-00ef-11eb-1e8b-b558dfaed82d
+# ╔═╡ 47199d10-0104-11eb-169d-6f9131a923ba
 my_graph
 
 # ╔═╡ Cell order:
@@ -90,16 +63,9 @@ my_graph
 # ╠═c4749610-00ce-11eb-1a2f-8f0e60e9c0a2
 # ╠═5f71b5d0-00cf-11eb-067c-6f7fafd83b3f
 # ╠═07312170-00d0-11eb-2214-5f83463faa58
-# ╠═b0f22570-00fc-11eb-1950-b95e857f6166
 # ╠═1734aa10-00da-11eb-297e-fd626ae614e5
 # ╠═3e2e1e60-00e6-11eb-1fdc-d5ceafc8de0b
 # ╠═d9f48190-00e6-11eb-2871-3f3c078263fc
 # ╠═b1329a90-00e0-11eb-1d3f-c9999ef689d7
-# ╠═3d2bad10-00e7-11eb-383b-5b97be2d9f26
-# ╠═f7244800-00db-11eb-1153-c3ed8515bf31
-# ╠═27e84270-00dc-11eb-1264-3bde81f1f815
-# ╠═11aa7bd0-00dd-11eb-250a-673e7c88ac11
-# ╠═27c87d10-00ea-11eb-232c-c3a8b25f4cf3
-# ╠═39d75c80-00e8-11eb-1667-db4d90329d47
-# ╠═91b20eb0-00dd-11eb-3c54-8b9b004e94e8
-# ╠═6d4fc460-00ef-11eb-1e8b-b558dfaed82d
+# ╠═279400c0-0104-11eb-30f2-23b812fd577f
+# ╠═47199d10-0104-11eb-169d-6f9131a923ba
