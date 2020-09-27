5be49aa4da17eff90736444ddbb8ee9b7700d301
louproul <70526480+louproul@users.noreply.github.com>
2020-09-27 19:17:10 -0400

    Update de report phase 1

--- projet/phase1/Rapport_phase1.jl
+++ projet/phase1/Rapport_phase1.jl
@@ -19,7 +19,10 @@ md"# Rapport de projet: Phase 1"
 md" Équipe: Elahe Amiri et Louis-Philippe Proulx"
 
 # ╔═╡ c4749610-00ce-11eb-1a2f-8f0e60e9c0a2
-md"Lien Github du code: [Branche Phase 1 du projet](https://www.google.com)"
+md"Lien Github du code: [Branche Phase 1 du projet](https://github.com/louproul/mth6412b-starter-code.git)"
+
+# ╔═╡ 893616e0-0110-11eb-1b68-5fada7e38619
+md" On pointe vers un des fichier sources de façon à ce que le code fonctionne sur différent poste de travail"
 
 # ╔═╡ 07312170-00d0-11eb-2214-5f83463faa58
 begin
@@ -30,17 +33,20 @@ begin
 	filepath = joinpath(filepath, filename_stsp) 
 end
 
+# ╔═╡ e66c8bf0-0110-11eb-1cee-f3e6d9cd495f
+md" Lecture du fichier, on retourne également un dictionnaire contenant les poids des arrêtes"
+
 # ╔═╡ 1734aa10-00da-11eb-297e-fd626ae614e5
 graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 
-# ╔═╡ 3e2e1e60-00e6-11eb-1fdc-d5ceafc8de0b
-Nodes = Node{Array{Float64,1}}[]
-
-# ╔═╡ d9f48190-00e6-11eb-2871-3f3c078263fc
-Edges = Edge{Array{Float64,1}}[]
+# ╔═╡ 3be53140-0111-11eb-026e-23e30d8d4dfd
+md" Création de notre graph vide. On lui donne le nom du fichier source (.tsp)"
 
 # ╔═╡ b1329a90-00e0-11eb-1d3f-c9999ef689d7
-my_graph = Graph(filename_stsp,Nodes,Edges)
+my_graph = Graph(filename_stsp,Node{Array{Float64,1}}[],Edge{Array{Float64,1}}[])
+
+# ╔═╡ 5b02bcee-0111-11eb-1e5d-bd361582a078
+md" On ajoute les noeuds et les arrête à notre objet \"my_graph\" en itérant sur les données récoltées du fichier"
 
 # ╔═╡ 279400c0-0104-11eb-30f2-23b812fd577f
 for k = 1 : length(graph_edges)
@@ -54,18 +60,24 @@ for k = 1 : length(graph_edges)
     end
 end
 
-# ╔═╡ 47199d10-0104-11eb-169d-6f9131a923ba
+# ╔═╡ dc9d0b7e-0116-11eb-184b-3fb24615e939
+
+
+# ╔═╡ d6584e90-0113-11eb-1aa1-dbea375c1fed
 my_graph
 
 # ╔═╡ Cell order:
-# ╠═396f4ab0-00ce-11eb-291f-cd5630db2bb7
-# ╠═a238b362-00ce-11eb-06bf-dbf6dce7add9
-# ╠═c4749610-00ce-11eb-1a2f-8f0e60e9c0a2
+# ╟─396f4ab0-00ce-11eb-291f-cd5630db2bb7
+# ╟─a238b362-00ce-11eb-06bf-dbf6dce7add9
+# ╟─c4749610-00ce-11eb-1a2f-8f0e60e9c0a2
 # ╠═5f71b5d0-00cf-11eb-067c-6f7fafd83b3f
+# ╟─893616e0-0110-11eb-1b68-5fada7e38619
 # ╠═07312170-00d0-11eb-2214-5f83463faa58
+# ╟─e66c8bf0-0110-11eb-1cee-f3e6d9cd495f
 # ╠═1734aa10-00da-11eb-297e-fd626ae614e5
-# ╠═3e2e1e60-00e6-11eb-1fdc-d5ceafc8de0b
-# ╠═d9f48190-00e6-11eb-2871-3f3c078263fc
+# ╟─3be53140-0111-11eb-026e-23e30d8d4dfd
 # ╠═b1329a90-00e0-11eb-1d3f-c9999ef689d7
+# ╟─5b02bcee-0111-11eb-1e5d-bd361582a078
 # ╠═279400c0-0104-11eb-30f2-23b812fd577f
-# ╠═47199d10-0104-11eb-169d-6f9131a923ba
+# ╠═dc9d0b7e-0116-11eb-184b-3fb24615e939
+# ╠═d6584e90-0113-11eb-1aa1-dbea375c1fed
