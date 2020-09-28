a3060923258e20cc444cd94ac811a604bdb65e04
Elahe Amiri <elahe.amiri@polymtl.ca>
2020-09-28 16:40:06 -0400

    change the path of opening data files and change the loop of creating graph to handle empty node

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -6,24 +6,37 @@ include("graph.jl")
 include("edge.jl")
 include("read_stsp.jl")
 
-md"# ___Rapport MTH6412B___"
+"""Access to data files"""
 
-filename_stsp = "bayg29.tsp"
-filepath = "D:\\Courses\\MTH6412B\\Project\\mth6412b-starter-code\\instances\\stsp\\"
+filename_stsp = "brazil58.tsp"
+root = normpath(joinpath(@__FILE__,"..","..",".."))
+filepath_to_stsp = "instances\\stsp"
+filepath = joinpath(root, filepath_to_stsp) 
 filepath = joinpath(filepath, filename_stsp) 
 
+"""Reading data form data files"""
 header = read_header(filepath)
 
 graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 
+"""Creating the Graph form the data"""
 
 Main_Graph = Graph("Graph_"*header["NAME"], Node{Array{Float64,1}}[], Edge{Array{Float64,1}}[]) 
 
+T = valtype(graph_nodes)
 for k = 1 : length(graph_edges)
-    new_node1 = Node(string(k),graph_nodes[k])
+    if isempty(graph_nodes)
+		new_node1 = Node{T}(string(k),T())
+	else
+   		new_node1 = Node(string(k),graph_nodes[k])
+	end
     add_node!(Main_Graph, new_node1)
     for j in graph_edges[k]
-        new_node2 = Node(string(j),graph_nodes[j])
+        if isempty(graph_nodes)
+	    	new_node2 = Node{T}(string(j),T())
+    	else
+		    new_node2 = Node(string(j),graph_nodes[j])
+	    end
         edge_name = "("*string(k)*","*string(j)*")"
         new_edge = Edge(edge_name, edges_weight[k,j], (new_node1 , new_node2))
         add_edge!(Main_Graph, new_edge)
