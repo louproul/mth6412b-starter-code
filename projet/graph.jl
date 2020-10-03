7eb8737b9beeed04759431c8fe98ff7efd318f67
louproul <70526480+louproul@users.noreply.github.com>
2020-10-03 11:03:15 -0400

    add the function to create_graph and to plot spanning trees

--- projet/graph.jl
+++ projet/graph.jl
@@ -62,3 +62,46 @@ function show(graph::Graph)
     show(edge)
   end
 end
+
+function create_graph!(graph::Graph, graph_nodes::Dict{Int64,Vector{Float64}},graph_edges::Array{Any,1}, edges_weight::Dict{Tuple{Int64,Int64},Float64})
+   T = valtype(graph_nodes)
+   for k = 1 : length(graph_edges)
+       if isempty(graph_nodes)
+       new_node1 = Node{T}(string(k),T())
+     else
+          new_node1 = Node(string(k),graph_nodes[k])
+     end
+       add_node!(graph, new_node1)
+       for j in graph_edges[k]
+           if isempty(graph_nodes)
+           new_node2 = Node{T}(string(j),T())
+         else
+           new_node2 = Node(string(j),graph_nodes[j])
+         end
+           edge_name = "("*string(k)*","*string(j)*")"
+           new_edge = Edge(edge_name, edges_weight[k,j], (new_node1 , new_node2))
+           add_edge!(graph, new_edge)
+       end
+   end
+end
+
+
+function plot_subgraph(graph::Graph,subset_edges::Set)
+
+  fig = plot(legend=false)
+
+  # edge positions
+  for edge in subset_edges
+      plot!([edge.adjacentnodes[1].data[1], edge.adjacentnodes[2].data[1]],
+      [edge.adjacentnodes[1].data[2], edge.adjacentnodes[2].data[2]],
+          linewidth=1.5, alpha=0.75, color=:lightgray)
+  end
+
+  # node positions
+  xys = graph.nodes
+  x = [xy.data[1] for xy in xys]
+  y = [xy.data[2] for xy in xys]
+  scatter!(x, y)
+
+  fig
+end
