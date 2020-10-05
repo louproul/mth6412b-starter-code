b2750ba36b969006f233fa79e1102de8d6d017a9
louproul <70526480+louproul@users.noreply.github.com>
2020-10-05 13:37:28 -0400

    For the report of phase 2

---
+++ projet/phase2/rapport_phase2.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 443ad950-06a5-11eb-1ebf-9fed2a75fa07
+md" # Rapport de projet: Phase 2"
+
+# ╔═╡ 58c70d70-06a6-11eb-2ce1-253886b97427
+md" Équipe: Elahe Amiri et Louis-Philippe Proulx"
+
+# ╔═╡ 47bbcd40-06a6-11eb-32bd-2b3bc3b16626
+md"Lien Github du code: [Branche Phase 2 du projet](https://github.com/louproul/mth6412b-starter-code.git)"
+
+# ╔═╡ 52d487ce-06a6-11eb-2482-dd65e447df8d
+
+
+# ╔═╡ 2cd338b0-06a6-11eb-3e54-0ff9884b75ac
+
+
+# ╔═╡ Cell order:
+# ╟─443ad950-06a5-11eb-1ebf-9fed2a75fa07
+# ╟─58c70d70-06a6-11eb-2ce1-253886b97427
+# ╟─47bbcd40-06a6-11eb-32bd-2b3bc3b16626
+# ╠═52d487ce-06a6-11eb-2482-dd65e447df8d
+# ╠═2cd338b0-06a6-11eb-3e54-0ff9884b75ac
