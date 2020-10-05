9d6f944652dd7670b6ef69633cfdc18f2983f876
louproul <70526480+louproul@users.noreply.github.com>
2020-10-05 18:10:46 -0400

    update to take into accounr the new type disjoint_set

--- projet/phase2/debug_disjoint.jl
+++ projet/phase2/debug_disjoint.jl
@@ -16,22 +16,15 @@ header = read_header(filepath)
 graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 
 """Creating the Graph form the data"""
-
-Main_Graph = Graph("Graph_"*header["NAME"], Node{Array{Float64,1}}[], Edge{Array{Float64,1}}[]) 
-
-
+Main_Graph = Graph{Vector{Float64}}() 
 create_graph!(Main_Graph, graph_nodes, graph_edges, edges_weight)
 
-A₀ = Set()
-k=0
-A = sort(Main_Graph.edges, by = x -> x.weight)
 
 my_disjoint_set = DisjointSet{Vector{Float64}}()
 
 create_disjointSet!(my_disjoint_set, Main_Graph)
 
-
-
+A = sort(Main_Graph.edges, by = x -> x.weight)
 
 for a in A
   if is_arc_disjoint(my_disjoint_set,a)
