98fb5a2549f9e5afad759648e525eab5f064203d
louproul <70526480+louproul@users.noreply.github.com>
2020-10-05 20:22:57 -0400

    test 3 graph creations

--- projet/phase2/graph_test.jl
+++ projet/phase2/graph_test.jl
@@ -2,13 +2,52 @@ using Test
 include("../graph.jl")
 include("../read_stsp.jl")
 
+
+filename_stsp = "pa561.tsp"
+root = normpath(joinpath(@__FILE__,"..","..",".."))
+filepath_to_stsp = "instances\\stsp"
+filepath = joinpath(root, filepath_to_stsp) 
+filepath = joinpath(filepath, filename_stsp) 
+
 my_graph = Graph{Vector{Float64}}() #The type of data containted in a node is a vector, (x,y) coordinate
-graph_nodes, graph_edges, edges_weight = read_stsp("instances/stsp/bayg29.tsp")
+graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
+create_graph!(my_graph, graph_nodes,graph_edges, edges_weight)
+
+
+@test nb_nodes(my_graph) == 561  # test that all nodes I've been added to the graph
+@test nb_edges(my_graph) == sum(1:1:561)
+
+
+filename_stsp = "bayg29.tsp"
+root = normpath(joinpath(@__FILE__,"..","..",".."))
+filepath_to_stsp = "instances\\stsp"
+filepath = joinpath(root, filepath_to_stsp) 
+filepath = joinpath(filepath, filename_stsp) 
+
+my_graph = Graph{Vector{Float64}}() #The type of data containted in a node is a vector, (x,y) coordinate
+graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 create_graph!(my_graph, graph_nodes,graph_edges, edges_weight)
 
 
 @test nb_nodes(my_graph) == 29  # test that all nodes I've been added to the graph
 @test nb_edges(my_graph) == sum(1:1:28)
+
+
+filename_stsp = "brazil58.tsp"
+root = normpath(joinpath(@__FILE__,"..","..",".."))
+filepath_to_stsp = "instances\\stsp"
+filepath = joinpath(root, filepath_to_stsp) 
+filepath = joinpath(filepath, filename_stsp) 
+
+my_graph = Graph{Vector{Float64}}() #The type of data containted in a node is a vector, (x,y) coordinate
+graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
+create_graph!(my_graph, graph_nodes,graph_edges, edges_weight)
+
+
+@test nb_nodes(my_graph) == 58  # test that all nodes I've been added to the graph
+@test nb_edges(my_graph) == sum(1:1:57)
+
+
 
 #= x = 3.14  # add and remove one element
 push!(s, x)
