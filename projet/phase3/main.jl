736fca9a555a62a5c57f963b65cd80f2997fe209
Elahe Amiri <elahe.amiri@polymtl.ca>
2020-11-01 22:58:55 -0500

    Adding the call for kruskal function with the two heuristics

--- projet/phase3/main.jl
+++ projet/phase3/main.jl
@@ -10,6 +10,7 @@ include("marked_node.jl")
 include("marked_edge.jl")
 include("marked_graph.jl")
 include("Prime_Alg.jl")
+include("kruskal_Alg.jl")
 
 
 """The main kruskal algorithm steps"""
@@ -25,12 +26,17 @@ header = read_header(filepath)
 
 graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
 
-"""Creating the Graph form the data"""
+"""Creating the marked Graph form the data"""
 
 Main_Graph = MarkedGraph("Graph_"*header["NAME"], MarkedNode{Array{Float64,1}}[], MarkedEdge{Array{Float64,1}}[]) 
-
 create_MarkedGraph!(Main_Graph, graph_nodes, graph_edges, edges_weight)
-W, MST_Graph = Prime_Algo(Main_Graph, edges_weight, Main_Graph.nodes[1])
 
-println(W)
-plot_markedgraph(MST_Graph)
+"""Calculating the minimum spanning tree using Prim algorithm"""
+W1, Prime_MST = Prime_Algo(Main_Graph, edges_weight, Main_Graph.nodes[1])
+println("The weight of MST using Prim Algorithm: ", W1)
+plot_markedgraph(Prime_MST)
+
+"""Calculating the minimum spanning tree using Kruskal algorithm"""
+W2, Kruskal_MST = Kruskal(Main_Graph)
+println("The weight of MST using Kruskal Algorithm: ", W2)
+plot_markedgraph(Kruskal_MST)
