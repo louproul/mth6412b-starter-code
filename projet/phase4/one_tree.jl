597c6b44b524065b7aea062908587858d8c0607e
Elahe Amiri <elahe.amiri@polymtl.ca>
2020-11-21 19:26:36 -0500

    defining a function to caluculate min_1tree

---
+++ projet/phase4/one_tree.jl
@@ -0,0 +1,50 @@
+using Markdown
+using InteractiveUtils
+
+
+include("../phase3/marked_node.jl")
+include("../phase3/marked_edge.jl")
+include("../phase3/marked_graph.jl")
+include("../phase3/Prime_Alg.jl")
+include("../phase3/kruskal_Alg.jl")
+
+
+function Minimum_1Tree(Graph::MarkedGraph{T},method::Int64) where T
+    second_NN = Dict()    # second nearest neighbours of graph nodes
+    for node in Graph.nodes
+        second_NN[node.name] = adjacent(node)[2][2]
+    end
+    
+    #finding the root vertex (that has the longest second nearest neighbor distance)
+    index = sort(collect(second_NN), by=x->x[2], rev=true)[1][1]
+    source = deepcopy(Graph.nodes[findfirst(x->x.name == index, Graph.nodes)])
+    
+    temp_Graph = deepcopy(Graph)
+    # removing the node from the graph
+    deleteat!(temp_Graph.nodes, findall(x->x.name==source.name, temp_Graph.nodes))
+    # removing the esges connected to that node
+    deleteat!(temp_Graph.edges, findall(x->x.adjacentnodes[1].name==source.name, temp_Graph.edges))
+    deleteat!(temp_Graph.edges, findall(x->x.adjacentnodes[2].name==source.name, temp_Graph.edges))
+    
+    
+    # calculating the minimm spaning tree
+    if method ==1
+        W, one_tree = Prime_Algo(temp_Graph, temp_Graph.nodes[rand(1:length(temp_Graph.nodes))])
+    elseif method ==2
+        W, one_tree = Kruskal(temp_Graph)
+    end
+    
+    #adding root to the minimum spaning tree
+    source_adj = deepcopy(adjacent(source))
+    empty!(source.adjacents)
+    for i in 1:2
+        node = one_tree.nodes[findfirst(x->x.name == source_adj[i][1], one_tree.nodes)]
+        edge_name =  "("*source.name*","*node.name*")"
+        new_edge = MarkedEdge(edge_name, source_adj[i][2], (source , node))
+        add_adj_node!(source, node, source_adj[i][2])
+        add_adj_node!(node, source, source_adj[i][2])
+        add_markededge!(one_tree, new_edge)
+    end
+    add_markednode!(one_tree, source)
+    return W, one_tree, source
+end
