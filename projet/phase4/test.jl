dff88203ae9a2aff4b0c7f4ca7ca8d599b752a34
Elahe Amiri <elahe.amiri@polymtl.ca>
2020-11-22 09:51:02 -0500

    create this file to test TSP_hk algorithm

---
+++ projet/phase4/test.jl
@@ -0,0 +1,38 @@
+using Markdown
+using InteractiveUtils
+
+include("../node.jl")
+include("../edge.jl")
+include("../graph.jl")
+include("../read_stsp.jl")
+
+include("../phase3/marked_node.jl")
+include("../phase3/marked_edge.jl")
+include("../phase3/marked_graph.jl")
+include("../phase3/Prime_Alg.jl")
+include("../phase3/kruskal_Alg.jl")
+
+include("one_tree.jl")
+include("TSP_hk.jl")
+
+
+filename_stsp = "bayg29.tsp"
+root = normpath(joinpath(@__FILE__,"..","..",".."))
+filepath_to_stsp = "instances\\stsp"
+filepath = joinpath(root, filepath_to_stsp) 
+filepath = joinpath(filepath, filename_stsp) 
+
+"""Reading data form data files"""
+header = read_header(filepath)
+
+graph_nodes, graph_edges, edges_weight = read_stsp(filepath)
+
+"""Creating the marked Graph form the data"""
+
+Main_Graph = MarkedGraph("Graph_"*header["NAME"], MarkedNode{Array{Float64,1}}[], MarkedEdge{Array{Float64,1}}[]) 
+create_MarkedGraph!(Main_Graph, graph_nodes, graph_edges, edges_weight)
+
+source = Calc_source(Main_Graph)
+
+W1, TSP_Graph = HK_MST(Main_Graph, 1 , source)
+plot_markedgraph(TSP_Graph)
