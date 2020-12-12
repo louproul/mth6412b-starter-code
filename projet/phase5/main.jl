7662db9847c4c58e6de19fd1eea57b84998a0d25
Elahe Amiri <elahe.amiri@polymtl.ca>
2020-12-12 18:17:42 -0500

    changes based on defining conver_TSP_to_img function

--- projet/phase5/main.jl
+++ projet/phase5/main.jl
@@ -19,7 +19,7 @@ include("read_stsp_new.jl")
 include("tools.jl")
 
 
-filename_stsp = "alaska-railroad.tsp"
+filename_stsp = "lower-kananaskis-lake.tsp"
 root = normpath(joinpath(@__FILE__,"..","..",".."))
 filepath_to_stsp = "instances\\img_stsp"
 filepath = joinpath(root, filepath_to_stsp) 
@@ -35,23 +35,25 @@ graph_nodes, graph_edges, edges_weight = read_img_stsp(filepath)
 Main_Graph = MarkedGraph("Graph_image", MarkedNode{Array{Float64,1}}[], MarkedEdge{Array{Float64,1}}[]) 
 create_img_Graph!(Main_Graph, graph_nodes, graph_edges, edges_weight)
 
-W2, HK_Graph = HK_MST(Main_Graph, 1, Main_Graph.nodes[1], 3, 1000) # method::Int64=0, t_step::Float64 = -1.0, stop_method::Int64 = 0)
+W2, HK_Graph = HK_MST(Main_Graph, 2, Main_Graph.nodes[1], 3, 10) # method::Int64=0, t_step::Float64 = -1.0, stop_method::Int64 = 0)
 println("The weight of TSP using HK algorithm: ", W2)
 
 New_TSP, new_W = create_tour!(deepcopy(HK_Graph), Main_Graph, W2)
 println("The weight of TSP Tour: ", new_W)
 
-new_W = Improve_path!(New_TSP, Main_Graph, new_W)
-println("The weight of TSP Tour: ", new_W)
 
-start_e = New_TSP.edges[findall(x->x.adjacentnodes[1].name == "1", New_TSP.edges)][1]
-Edge_list = create_touredge_list!(New_TSP, start_e)
-node_tour = Tour_nodes_list(New_TSP)
+#Improved_W = Improve_path!(New_TSP, Main_Graph, new_W)
+#println("The final weight of TSP Tour: ", Improved_W)
 
-input_img = "alaska-railroad.png"
+#start_e = New_TSP.edges[findall(x->x.adjacentnodes[1].name == "1", New_TSP.edges)][1]
+#Edge_list = create_touredge_list!(New_TSP, start_e)
+#node_tour = Tour_nodes_list(New_TSP)
+
+input_img = "lower-kananaskis-lake.png"
 inputpath_to_stsp = "images\\shuffled"
 inputpath = joinpath(root, inputpath_to_stsp) 
 inputpath = joinpath(inputpath, input_img) 
 
-write_tour("alaska-railroad.tour", node_tour, convert(Float32, W2))
-reconstruct_picture("alaska-railroad.tour", inputpath, "constructed_alaska-railroad.png"; view = true)
+#write_tour("lower-kananaskis-lake.tour", node_tour, convert(Float32, W2))
+conver_TSP_to_img(New_TSP ,"lower-kananaskis-lake.tour")
+reconstruct_picture("lower-kananaskis-lake.tour", inputpath, "constructed_lower-kananaskis-lake.png"; view = true)
